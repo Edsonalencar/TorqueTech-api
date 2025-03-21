4e13ccf2f3455dfefa5d09ad812d5179b8e1b613
João Neto <jnetogoncalo@gmail.com>
2025-03-21 15:11:56 -0300

    Create.  Service for users garage

---
+++ src/main/java/br/com/starter/domain/usersGarages/UsersGarageService.java
@@ -0,0 +1,93 @@
+package br.com.starter.domain.usersGarages;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UsersGarageService {
+
+    private final UsersGarageRepository usersGarageRepository;
+
+    public UsersGarage save(UsersGarage usersGarage) {
+        return usersGarageRepository.save(usersGarage);
+    }
+
+    public UsersGarage getById(UUID id) {
+        return usersGarageRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Relação não encontrada"));
+    }
+
+    public Page<UsersGarage> findByUser(UUID userId, Pageable pageable) {
+        return usersGarageRepository.findByUser(userId, pageable);
+    }
+
+    public Page<UsersGarage> findByGarage(UUID garageId, Pageable pageable) {
+        return usersGarageRepository.findByGarage(garageId, pageable);
+    }
+
+    public Page<UsersGarage> findPrimaryGarageByGarage(UUID garageId, Pageable pageable) {
+        return usersGarageRepository.findPrimaryGarageByGarage(garageId, pageable);
+    }
+
+    public UsersGarage findPrimaryByUser(UUID userId) {
+        return usersGarageRepository.findPrimaryByUser(userId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Nenhuma garagem principal encontrada para este usuário"));
+    }
+
+    public boolean isUserGarage(UUID userId, UUID garageId) {
+        return usersGarageRepository.isUserGarage(userId, garageId);
+    }
+
+    public boolean isUserGaragePrimary(UUID userId, UUID garageId) {
+        return usersGarageRepository.isUserGaragePrimary(userId, garageId);
+    }
+
+    public List<UsersGarage> getAll() {
+        return usersGarageRepository.findAll();
+    }
+
+    @Transactional
+    public UsersGarage update(UUID id, boolean isPrimary) {
+        UsersGarage current = usersGarageRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Relação não encontrada"));
+
+        if (isPrimary) {
+            usersGarageRepository.findPrimaryByUser(current.getUser().getId())
+                    .ifPresent(primary -> {
+                        primary.setPrimary(false);
+                        primary.setIsPrimaryEdit(LocalDateTime.now());
+                        usersGarageRepository.save(primary);
+                    });
+            current.setPrimary(isPrimary);
+        } else
+            current.setPrimary(isPrimary);
+        current.setIsPrimaryEdit(LocalDateTime.now());
+
+        return usersGarageRepository.save(current);
+    }
+
+    @Transactional
+    public boolean deleteById(UUID id) {
+        if (!usersGarageRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Relação UsersGarage não encontrada");
+        } 
+        if (usersGarageRepository.findById(id).get().isPrimary()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não é possível excluir a relação UsersGarage principal");
+        }
+
+        usersGarageRepository.deleteById(id);
+        return true;
+    }
+}
