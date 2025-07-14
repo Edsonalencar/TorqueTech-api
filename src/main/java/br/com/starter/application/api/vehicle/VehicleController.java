264d92bc4ef245619478105294d4685ec074dc9e
Carlos Magno II <carlos.ramos@alunos.ufersa.edu.br>
2025-07-14 17:09:42 -0300

    FEAT

--- src/main/java/br/com/starter/application/api/vehicle/VehicleController.java
+++ src/main/java/br/com/starter/application/api/vehicle/VehicleController.java
@@ -1,48 +1,48 @@
-package br.com.starter.application.api.vehicle;
+    package br.com.starter.application.api.vehicle;
 
-import br.com.starter.application.api.common.GetPageRequest;
-import br.com.starter.application.api.common.ResponseDTO;
-import br.com.starter.application.api.vehicle.dtos.CreateVehicleDTO;
-import br.com.starter.application.api.vehicle.dtos.UpdateVehicleDTO;
-import br.com.starter.application.useCase.vehicle.*;
-import br.com.starter.domain.customer.Customer;
-import br.com.starter.domain.customer.CustomerService;
-import br.com.starter.domain.user.CustomUserDetails;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+    import br.com.starter.application.api.common.GetPageRequest;
+    import br.com.starter.application.api.common.ResponseDTO;
+    import br.com.starter.application.api.vehicle.dtos.CreateVehicleDTO;
+    import br.com.starter.application.api.vehicle.dtos.UpdateVehicleDTO;
+    import br.com.starter.application.useCase.vehicle.*;
+    import br.com.starter.domain.customer.Customer;
+    import br.com.starter.domain.customer.CustomerService;
+    import br.com.starter.domain.user.CustomUserDetails;
+    import jakarta.validation.Valid;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.security.core.annotation.AuthenticationPrincipal;
+    import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-import java.util.UUID;
+    import java.util.Optional;
+    import java.util.UUID;
 
-@RestController
-@RequestMapping("/torque/api/vehicle")
-@RequiredArgsConstructor
-public class VehicleController {
+    @RestController
+    @RequestMapping("/torque/api/vehicle")
+    @RequiredArgsConstructor
+    public class VehicleController {
 
-    private final CreateVehicleUseCase createVehicleUseCase;
-    private final GetPageVehicleUseCase getPageVehicleUseCase;
-    private final GetVehicleUseCase getVehicleUseCase;
-    private final GetPageVehicleByCustomerUseCase getPageVehicleByCustomerUseCase;
-    private final UpdateVehicleUseCase updateVehicleUseCase;
-    private final GetAllVehiclesByCustomerUseCase getAllVehiclesUseCase;
+        private final CreateVehicleUseCase createVehicleUseCase;
+        private final GetPageVehicleUseCase getPageVehicleUseCase;
+        private final GetVehicleUseCase getVehicleUseCase;
+        private final GetPageVehicleByCustomerUseCase getPageVehicleByCustomerUseCase;
+        private final UpdateVehicleUseCase updateVehicleUseCase;
+        private final GetAllVehiclesByCustomerUseCase getAllVehiclesUseCase;
 
 
-    @PostMapping
-    public ResponseEntity<?> create(
-            @AuthenticationPrincipal CustomUserDetails userAuthentication,
-            @Valid @RequestBody CreateVehicleDTO request
-    ) {
-        return ResponseEntity.ok(
-                new ResponseDTO<>(
-                        createVehicleUseCase.handler(request, userAuthentication.getUser())
-                )
-        );
-    }
+        @PostMapping
+        public ResponseEntity<?> create(
+                @AuthenticationPrincipal CustomUserDetails userAuthentication,
+                @Valid @RequestBody CreateVehicleDTO request
+        ) {
+            return ResponseEntity.ok(
+                    new ResponseDTO<>(
+                            createVehicleUseCase.handler(request, userAuthentication.getUser())
+                    )
+            );
+        }
 
 
     @PutMapping("/{vehicleId}")
